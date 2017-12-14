905e5c59e405dcf8bf155287b00005e0d3f96e01
Sven Wirner <svenwirner@Philipps-MBP-3.fritz.box>
2017-12-14 11:05:06 +0100

    0.8

--- Player.js
+++ Player.js
@@ -1,6 +1,6 @@
 class Player {
   static get VERSION() {
-    return '0.7.1';
+    return '0.8';
   }
 
   static betRequest(gameState, bet) {
@@ -8,14 +8,20 @@ class Player {
     var player = gameState.players[gameState.in_action];
     var buyin = gameState.current_buy_in;
     var minraise = gameState.minimum_raise;
+    var round = gameState.round;
 
     var minimumRaise = buyin - player.bet + minraise;
     var call = buyin - player.bet;
 
     var card1 = player.hole_cards[0];
     var card2 = player.hole_cards[1];
-
-    if ((card1.rank === "K" || card1.rank === "A" || card1.rank === "Q" || card1.rank === "J") && (card1.rank === card2.rank)) {
+    
+    if((card1.rank === 'A' && card2.rank === 'A') 
+    || (card1.rank === 'K' && card2.rank === 'K')
+    || (card1.rank === 'Q' && card2.rank === 'Q')
+    || (card1.rank === 'J' && card2.rank === 'J')
+    || (card1.rank === 'A' && card2.rank === 'K' && card1.suit === card2.suit)
+    || (card1.rank === 'K' && card2.rank === 'A' && card1.suit === card2.suit)) {
       bet(minimumRaise);
     } else if (card1.rank === card2.rank) {
       bet(call);
