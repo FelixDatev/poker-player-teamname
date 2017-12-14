7e1bcecff25c5a3de2c1ccb313223bbf27d8e7b4
Sven Wirner <svenwirner@Philipps-MBP-3.fritz.box>
2017-12-14 15:51:37 +0100

    2.0

--- Player.js
+++ Player.js
@@ -1,9 +1,10 @@
 class Player {
   static get VERSION() {
-    return '1.338';
+    return '2.0';
   }
 
   static betRequest(gameState, bet) {
+
     var player = gameState.players[gameState.in_action];
     var community_cards = gameState.community_cards;
 
@@ -13,10 +14,46 @@ class Player {
     var card1 = player.hole_cards[0];
     var card2 = player.hole_cards[1];
 
-    if ((card1.rank === "A" || card1.rank === "K") && (card1.rank === card2.rank)) {
-      bet(minimumRaise);
+    if(community_cards.length == 0) {
+      // Pre-Flop
+      if ((card1.rank === "A" || card1.rank === "K" || card1.rank === "Q" || card1.rank === "J") && (card1.rank === card2.rank)) {
+        bet(minimumRaise);
+      } else if (card1.rank === card2.rank) {
+        bet(call);
+      } else if (card1.rank === "A" || card1.rank === "K" || card1.rank === "Q" || card1.rank === "J" || card2.rank === "A" || card2.rank === "K" || card2.rank === "Q" || card2.rank === "J") {
+        bet(call);
+      } else {
+        bet(0);
+      }
     } else {
-      bet(0);
+      // Post-Flop
+      var rate = 0;
+
+      if (card1.rank === "A" || card1.rank === "K" || card1.rank === "Q" || card2.rank === "A" || card2.rank === "K" || card2.rank === "Q") {
+        rate++;
+      }
+      
+      if (card1.rank === card2.rank) {
+        rate = rate + 2;
+      } 
+
+      community_cards.forEach(function(card) {
+        if(card.rank === card1.rank || card.rank === card2.rank) {
+          rate++;
+          if(card.rank === "A" || card.rank === "K" || card.rank === "Q" || card.rank === "J") {
+            rate++;
+          }
+        }
+      });
+
+      console.log("Rate: " + rate);
+      if(rate > 2) {
+        bet(minimumRaise);
+      } else if(rate > 0) {
+        bet(call);
+      } else {
+        bet(0);
+      }
     }
   }
 
