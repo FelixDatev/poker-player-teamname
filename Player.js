2a7d31a80299d5b39e0e42b4dc502012688dcc9e
Felix Fischer <felix.fischer.datev@gmail.com>
2017-12-14 10:20:00 +0100

    ; bet 100

--- Player.js
+++ Player.js
@@ -7,21 +7,24 @@ class Player {
 
     var player = gameState.players[gameState.in_action];
 
-    var minimumRaise = gameState.current_buy_in - player.bet + gameState.minimum_raise + 1000;
+    var minimumRaise = gameState.current_buy_in - player.bet + gameState.minimum_raise;
     var call = gameState.current_buy_in - player.bet;
 
     var holeCard1 = player.hole_cards[0];
     var holeCard2 = player.hole_cards[1];
 
-bet(10001);
 
-    if (holeCard1.rank === "K" || holeCard1.rank === "A" || holeCard1.rank === "Q" || holeCard1.rank === "J") {
+if (holeCard1.rank === "K" || holeCard1.rank === "A" || holeCard1.rank === "Q" || holeCard1.rank === "J") {
+  if (holeCard1.rank === holeCard2.rank) {
+    bet(100);
+  }
+}
+
       if (holeCard1.rank === holeCard2.rank) {
-        //bet(minimumRaise);
+        bet(minimumRaise);
       } else {
-        //bet(call);
+        bet(call);
       }
-    }
 
   }
 
