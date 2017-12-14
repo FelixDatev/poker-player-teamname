3022958caba7454cea8b3e0a915b9b72178bf394
Sven Wirner <svenwirner@Philipps-MBP-3.fritz.box>
2017-12-14 14:11:35 +0100

    0.40

--- Player.js
+++ Player.js
@@ -1,6 +1,6 @@
 class Player {
   static get VERSION() {
-    return '0.39';
+    return '0.40';
   }
 
   static betRequest(gameState, bet) {
@@ -17,8 +17,8 @@ class Player {
     if(community_cards.length == 0) {
       // Pre-Flop
       if ((card1.rank === "A" || card1.rank === "K" || card1.rank === "Q" || card1.rank === "J") && (card1.rank === card2.rank)) {
-        bet(minimumRaise);
-      } else if (card1.rank === card2.rank) {
+        bet(call);
+      } else if (card1.rank === card2.rank && gameState.current_buy_in < 100) {
         bet(call);
       } else {
         bet(0);
