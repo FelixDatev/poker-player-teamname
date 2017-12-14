ac7a22371129fe8c90f3d27102ae4b3a3c1de920
Sven Wirner <svenwirner@Philipps-MBP-3.fritz.box>
2017-12-14 15:39:06 +0100

    1.337

--- Player.js
+++ Player.js
@@ -1,10 +1,10 @@
 class Player {
   static get VERSION() {
-    return '1.0';
+    return '1.337';
   }
 
   static betRequest(gameState, bet) {
-
+    console.log("gameState: " +  JSON.stringify(gameState));
     var player = gameState.players[gameState.in_action];
     var community_cards = gameState.community_cards;
 
@@ -17,10 +17,12 @@ class Player {
     if(community_cards.length == 0) {
       // Pre-Flop
       if ((card1.rank === "A" || card1.rank === "K" || card1.rank === "Q" || card1.rank === "J") && (card1.rank === card2.rank)) {
-        bet(minimumRaise);
-      } else if (card1.rank === card2.rank) {
         bet(call);
-      } else if ((card1.rank === "A" || card1.rank === "K" || card1.rank === "Q" || card1.rank === "J") && ( card2.rank === "A" || card2.rank === "K" || card2.rank === "Q" || card2.rank === "J")) {
+      } else if (card1.rank === card2.rank && gameState.current_buy_in < 100) {
+        bet(call);
+      } else if ((card1.rank === "A" || card1.rank === "K" || card1.rank === "Q" || card1.rank === "J") 
+              && (card2.rank === "A" || card2.rank === "K" || card2.rank === "Q" || card2.rank === "J")
+              && gameState.current_buy_in < 50) {
         bet(call);
       } else {
         bet(0);
@@ -29,30 +31,62 @@ class Player {
       // Post-Flop
       var rate = 0;
 
-      if (card1.rank === "A" || card1.rank === "K" || card1.rank === "Q" || card2.rank === "A" || card2.rank === "K" || card2.rank === "Q") {
+      if ((card1.rank === "A" && card2.rank === "K") || (card1.rank === "K" && card2.rank === "A")) {
         rate++;
       }
-      
-      if (card1.rank === card2.rank && card1.rank !== "2" && card1.rank !== "3") {
-        rate = rate + 2;
-      } 
 
+      // Pairs
+      if((card1.rank === "A" && card2.rank === "A")
+      || (card1.rank === "K" && card2.rank === "K")
+      || (card1.rank === "Q" && card2.rank === "Q")
+      || (card1.rank === "J" && card2.rank === "J")) {
+        rate++;
+        rate++;
+      }
+
+      var pair = 0;
       community_cards.forEach(function(card) {
         if(card.rank === card1.rank || card.rank === card2.rank) {
           rate++;
-          if(card.rank === "A" || card.rank === "K" || card.rank === "Q" || card.rank === "J") {
-            rate++;
-          }
         }
       });
 
-      console.log("Rate: " + rate);
+      // Flush
+      var hearts = 0;
+      var spades = 0;
+      var clubs = 0;
+      var diamonds = 0;
+      [card1, card2].concat(community_cards).forEach(function(card) {
+          if(card.suit == "hearts") {
+            hearts++;
+          } else if(card.suit == "spades") {
+            spades++;
+          } else if(card.suit == "clubs") {
+            clubs++;
+          } else {
+            diamonds++;
+          }
+      });
+
+      if(hearts > 3 || spades > 3 || clubs > 3 || diamonds > 3) {
+        rate++;
+      } else if(hearts > 4 || spades > 4 || clubs > 4 || diamonds > 4) {
+        rate = 3;
+      }
+
+      // Rating
       if(rate > 2) {
+        bet(minimumRaise*2);
+      } else if(rate > 1 && gameState.current_buy_in < 50) {
         bet(minimumRaise);
-      } else if(rate > 0) {
+      } else if(rate > 0 && gameState.current_buy_in < 50) {
         bet(call);
       } else {
-        bet(0);
+        if(community_cards.length > 3 && gameState.current_buy_in < 50) {
+          bet(call);
+        } else {
+          bet(0);
+        }
       }
     }
   }
