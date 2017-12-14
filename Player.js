721ed1b1d6d19a9f837eb4cc33c3eeef60649922
Sven Wirner <svenwirner@Philipps-MBP-3.fritz.box>
2017-12-14 11:06:23 +0100

    0.9

--- Player.js
+++ Player.js
@@ -1,6 +1,6 @@
 class Player {
   static get VERSION() {
-    return '0.8';
+    return '0.9';
   }
 
   static betRequest(gameState, bet) {
@@ -8,27 +8,31 @@ class Player {
     var player = gameState.players[gameState.in_action];
     var buyin = gameState.current_buy_in;
     var minraise = gameState.minimum_raise;
-    var round = gameState.round;
+    var community_cards = gameState.community_cards;
 
     var minimumRaise = buyin - player.bet + minraise;
     var call = buyin - player.bet;
 
     var card1 = player.hole_cards[0];
     var card2 = player.hole_cards[1];
-    
-    if((card1.rank === 'A' && card2.rank === 'A') 
-    || (card1.rank === 'K' && card2.rank === 'K')
-    || (card1.rank === 'Q' && card2.rank === 'Q')
-    || (card1.rank === 'J' && card2.rank === 'J')
-    || (card1.rank === 'A' && card2.rank === 'K' && card1.suit === card2.suit)
-    || (card1.rank === 'K' && card2.rank === 'A' && card1.suit === card2.suit)) {
-      bet(minimumRaise);
-    } else if (card1.rank === card2.rank) {
-      bet(call);
-    } else if (card1.rank === "K" || card1.rank === "A" || card1.rank === "Q" || card1.rank === "J") {
-      bet(call);
+
+    if(community_cards.length == 0) {
+      if((card1.rank === 'A' && card2.rank === 'A') 
+      || (card1.rank === 'K' && card2.rank === 'K')
+      || (card1.rank === 'Q' && card2.rank === 'Q')
+      || (card1.rank === 'J' && card2.rank === 'J')
+      || (card1.rank === 'A' && card2.rank === 'K' && card1.suit === card2.suit)
+      || (card1.rank === 'K' && card2.rank === 'A' && card1.suit === card2.suit)) {
+        bet(minimumRaise);
+      } else if (card1.rank === card2.rank) {
+        bet(call);
+      } else if (card1.rank === "K" || card1.rank === "A" || card1.rank === "Q" || card1.rank === "J") {
+        bet(call);
+      } else {
+        bet(0);
+      }
     } else {
-      bet(0);
+      bet(call);
     }
   }
 
