ceecbe191e210bb0299dc4889c316cccaad9288e
Felix Fischer <felix.fischer.datev@gmail.com>
2017-12-14 10:38:52 +0100

    groups

--- Player.js
+++ Player.js
@@ -22,6 +22,35 @@ class Player {
     } else {
       bet(0);
     }
+
+    if((card1.rank === 'A' && card2.rank === 'A')
+        || (card1.rank === 'K' && card2.rank === 'K')
+        || (card1.rank === 'A' && card2.rank === 'K' && card1.suit === card2.suit)
+        || (card1.rank === 'K' && card2.rank === 'A' && card1.suit === card2.suit)) {
+      group = 'A';
+    } else if((card1.rank === 'Q' && card2.rank === 'Q')
+        || (card1.rank === 'A' && card2.rank === 'K')
+        || (card1.rank === 'K' && card2.rank === 'A')
+        || (card1.rank === 'J' && card2.rank === 'J')
+        || (card1.rank === '10' && card2.rank === '10')) {
+      group = 'B';
+    } else if((card1.rank === 'A' && card2.rank === 'Q')
+        || (card1.rank === 'Q' && card2.rank === 'A')
+        || (card1.rank === '9' && card2.rank === '9')
+        || (card1.rank === '8' && card2.rank === '8')
+        || (card1.rank === 'A' && card2.rank === 'J')
+        || (card1.rank === 'J' && card2.rank === 'A')) {
+      group = 'C';
+    } else if((card1.rank === '7' && card2.rank === '7')
+        || (card1.rank === 'K' && card2.rank === 'Q' && card1.suit === card2.suit)
+        || (card1.rank === 'Q' && card2.rank === 'K' && card1.suit === card2.suit)
+        || (card1.rank === '6' && card2.rank === '6')
+        || (card1.rank === 'A' && card2.rank === '10' && card1.suit === card2.suit)
+        || (card1.rank === '10' && card2.rank === 'A' && card1.suit === card2.suit)
+        || (card1.rank === '5' && card2.rank === '5')) {
+      group = 'D';
+    }
+    
   }
 
   static showdown(gameState) {
